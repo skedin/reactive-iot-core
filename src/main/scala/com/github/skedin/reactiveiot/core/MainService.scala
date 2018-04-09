39cbc52dc8e0fb07565c99d3785ca2fa118b1f2f
Stephen Kuo <stephenkuo@foxmail.com>
2018-04-09 14:20:00 +0800

    Initial commit

---
+++ src/main/scala/com/github/skedin/reactiveiot/core/MainService.scala
@@ -0,0 +1,10 @@
+package com.github.skedin.reactiveiot.core
+
+import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
+
+
+object MainService extends App {
+
+  val system: ActorSystem = ActorSystem("Reactive-IoT-Core")
+
+}
